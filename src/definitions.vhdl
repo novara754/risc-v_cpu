1d320ef5cfa264126e5f98883c1956a8d9b4f97d
vzwGrey <vzwgrey@gmail.com>
2022-08-29 19:33:08 +0200

    feat(memory): allow writing bytes, half-words and words

--- src/definitions.vhdl
+++ src/definitions.vhdl
@@ -3,6 +3,7 @@ use ieee.numeric_std.all;
 
 package risc_v is
 	subtype t_register_index is unsigned(4 downto 0);
+	subtype t_byte is unsigned(7 downto 0);
 	subtype t_data is unsigned(31 downto 0);
 	subtype t_address is unsigned(31 downto 0);
 end package risc_v;
--- tb/memory_tb.vhdl
+++ tb/memory_tb.vhdl
@@ -12,8 +12,9 @@ end memory_tb;
 architecture rtl of memory_tb is
 	signal r_clk : std_logic := '0';
 	signal r_inst_address : t_address := (others => '0');
-	signal r_data_address : t_address := (others => '0');
+	signal r_data_address : t_address := X"0000_0004";
 	signal r_in_data : t_data := (others => '0');
+	signal r_write_mask : std_logic_vector(3 downto 0) := (others => '0');
 	signal r_write_enable : std_logic := '0';
 	signal w_instruction : t_data;
 	signal w_out_data : t_data;
@@ -24,6 +25,7 @@ begin
 			i_inst_address => r_inst_address,
 			i_data_address => r_data_address,
 			i_in_data => r_in_data,
+			i_write_mask => r_write_mask,
 			i_write_enable => r_write_enable,
 			o_instruction => w_instruction,
 			o_out_data => w_out_data
@@ -41,21 +43,23 @@ begin
 	begin
 		-- TEST WRITE ONLY IF WRITE ENABLE ASSERTED --
 		report "WRITE ONLY IF WRITE ENABLE ASSERTED" severity note;
-		r_data_address <= (others => '0');
+		r_data_address <= X"0000_0004";
 		r_in_data <= X"0000_4321";
+		r_write_mask <= "1111";
 		r_write_enable <= '0';
 		wait until (r_clk = '1');
 
 		r_data_address <= (others => '0');
 		wait for c_SHORT_TIME;
 		assert (w_out_data = X"0000_0000")
-			report time'image(now) & " test 1 failed " & integer'image(to_integer(w_out_data)) severity failure;
+			report "test 1 failed" severity failure;
 		----------------------------------------------
 
-		-- TEST SIMPLE WRITE AND READ BACK --
-		report "SIMPLE WRITE AND READ BACK" severity note;
+		-- TEST WRITE WORD AND READ BACK --
+		report "WRITE WORD AND READ BACK" severity note;
 		r_data_address <= (others => '0');
 		r_in_data <= X"3E80_0093";
+		r_write_mask <= "1111";
 		r_write_enable <= '1';
 		wait until (r_clk = '1');
 		r_write_enable <= '0';
@@ -63,22 +67,53 @@ begin
 		r_data_address <= (others => '0');
 		wait for c_SHORT_TIME;
 		assert (w_out_data = X"3E80_0093")
-			report time'image(now) & " test 2 failed" severity failure;
+			report "test 2 failed" severity failure;
 		-------------------------------------
+
+		-- TEST WRITE HALF-WORD AND READ BACK WORD --
+		report "WRITE HALF-WORD AND READ BACK WORD" severity note;
+		r_data_address <= (others => '0');
+		r_in_data <= X"0000_6AF0";
+		r_write_mask <= "0011";
+		r_write_enable <= '1';
+		wait until (r_clk = '1');
+		r_write_enable <= '0';
+
+		r_data_address <= (others => '0');
+		wait for c_SHORT_TIME;
+		assert (w_out_data = X"3E80_6AF0")
+			report "test 3 failed" severity failure;
+		---------------------------------------------
 
 		-- TEST SECOND WRITE AND READ BACK --
 		report "SECOND WRITE AND READ BACK" severity note;
-		r_data_address(0) <= '1';
+		r_data_address <= X"0000_0004";
 		r_in_data <= X"8301_8213";
+		r_write_mask <= "1111";
 		r_write_enable <= '1';
 		wait until (r_clk = '1');
 		r_write_enable <= '0';
 
 		r_inst_address <= (others => '0');
 		wait for c_SHORT_TIME;
-		assert (w_out_data = X"8301_8213" and w_instruction = X"3E80_0093")
-			report time'image(now) & " test 3 failed" severity failure;
+		assert (w_out_data = X"8301_8213" and w_instruction = X"3E80_6AF0")
+			report "test 4 failed" severity failure;
 		-------------------------------------
+
+		-- TEST WRITE BYTE AND READ BACK WORD --
+		report "WRITE BYTE AND READ BACK WORD" severity note;
+		r_data_address <= (others => '0');
+		r_in_data <= X"009A_0000";
+		r_write_mask <= "0100";
+		r_write_enable <= '1';
+		wait until (r_clk = '1');
+		r_write_enable <= '0';
+
+		r_data_address <= (others => '0');
+		wait for c_SHORT_TIME;
+		assert (w_out_data = X"3E9A_6AF0")
+			report "test 5 failed" severity failure;
+		----------------------------------------
 
 		report "ALL TESTS FINISHED" severity note;
 
