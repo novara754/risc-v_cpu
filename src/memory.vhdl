aa95a8823ad1960eb7a5f99330c38c099cc70e7c
vzwGrey <vzwgrey@gmail.com>
2022-08-30 15:29:01 +0200

    feat(memory): allow initializing memory from file

--- src/memory.vhdl
+++ src/memory.vhdl
@@ -17,8 +17,17 @@ use work.risc_v.all;
 -- different sizes, be it a byte, half-word or word. This is used for the SW, SH
 -- and SB instructions.
 --
+-- The memory module allows initialization from a file. This file must follow the
+-- following format:
+--  * Text file
+--  * One line per 32-bit value
+--  * Each value encoded as a hexadecimal number
+--
 -- At the moment this memory module only supports addresses 1024 words of data.
 entity memory is
+	generic (
+		g_source_file_name : string := ""
+	);
 	port (
 		-- Clock signal
 		i_clk : in std_logic;
@@ -43,7 +52,36 @@ end memory;
 
 architecture rtl of memory is
 	type t_memory is array(0 to 1023) of t_data;
-	signal r_memory : t_memory := (others => (others => '0'));
+
+	impure function read_file (source_file_name : in string) return t_memory is
+		use std.textio.all;
+		file source_file : text is in source_file_name;
+		variable source_line : line;
+		variable word : std_logic_vector(31 downto 0);
+		variable program : t_memory;
+	begin
+		for i in t_memory'range loop
+			if not endfile(source_file) then
+				readline(source_file, source_line);
+				hread(source_line, word);
+				program(i) := unsigned(word);
+			else
+				program(i) := (others => '0');
+			end if;
+		end loop;
+		return program;
+	end function;
+
+	impure function init_memory return t_memory is
+	begin
+		if g_source_file_name = "" then
+			return (others => (others => '0'));
+		else
+			return read_file(g_source_file_name);
+		end if;
+	end function;
+
+	signal r_memory : t_memory := init_memory;
 
 	signal w_inst_offset : unsigned(29 downto 0);
 	signal w_data_offset : unsigned(29 downto 0);
